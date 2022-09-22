f33833ce1bdfdd800f7aac2cae423b19c6049126
Douglas-VC <douglasvcoutinho@hotmail.com>
2022-09-22 19:26:39 -0300

    Functions for graph coordinates and cost matrix calculation.

--- DijkstraPathPlanning.cpp
+++ DijkstraPathPlanning.cpp
@@ -14,6 +14,7 @@ using namespace std::chrono;
 
 typedef pcl::PointCloud<pcl::PointXYZ>::Ptr PointCloud;
 typedef pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> Octree;
+typedef coder::array<float, 2> Matrix;
 
 PointCloud importPointCloud() {
 
@@ -40,15 +41,15 @@ Octree transformPointCloudToOctree(const PointCloud &cloud, float resolution) {
     return octree;
 }
 
-void getMatrixDimensions(const PointCloud &cloud, int &rows, int &columns, float resolution) {
+void getMatrixDimensions(const PointCloud &cloud, int &rows, int &columns, float graphResolution) {
     pcl::PointXYZ minPt, maxPt;
     pcl::getMinMax3D(*cloud, minPt, maxPt);
 
-    rows = floor(fabs((double) (maxPt.x - minPt.x)) / resolution);
-    columns = floor(fabs((double) (maxPt.y - minPt.y)) / resolution);
+    rows = floor(fabs((double) (maxPt.x - minPt.x)) / graphResolution) + 1;
+    columns = floor(fabs((double) (maxPt.y - minPt.y)) / graphResolution) + 1;
 }
 
-void populateAdjacencyMatrix(coder::array<float, 2> &AdjacencyMatrix, int rows, int columns) {
+void populateAdjacencyMatrix(Matrix &AdjacencyMatrix, int rows, int columns) {
     int N = rows * columns;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -65,22 +66,61 @@ void populateAdjacencyMatrix(coder::array<float, 2> &AdjacencyMatrix, int rows, 
     }
 }
 
-void getKNearestNeighboursFromOctree(Octree octree, const PointCloud &cloud, int K, pcl::PointXYZ searchPoint) {
+std::vector<double> makeStepVector(double beg, double step, double end) {
+    std::vector<double> vec;
+    vec.reserve(fabs((end - beg)) / step + 1);
+    while (beg <= end) {
+        vec.push_back(beg);
+        beg += step;
+    }
+    return vec;
+}
 
+void getGraphCoordinates(Octree octree, const PointCloud &cloud, Matrix &graphCoordinates, int rows,
+                         int columns,
+                         float graphResolution) {
+    pcl::PointXYZ minPt, maxPt;
     std::vector<int> pointIdxNKNSearch;
     std::vector<float> pointNKNSquaredDistance;
 
-    std::cout << "K nearest neighbor search at (" << searchPoint.x
-              << " " << searchPoint.y
-              << " " << searchPoint.z
-              << ") with K=" << K << std::endl;
+    pcl::getMinMax3D(*cloud, minPt, maxPt);
 
-    if (octree.nearestKSearch(searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0) {
-        for (std::size_t i = 0; i < pointIdxNKNSearch.size(); ++i)
-            std::cout << "    " << (*cloud)[pointIdxNKNSearch[i]].x
-                      << " " << (*cloud)[pointIdxNKNSearch[i]].y
-                      << " " << (*cloud)[pointIdxNKNSearch[i]].z
-                      << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
+    auto iValues = makeStepVector(minPt.x, graphResolution, maxPt.x);
+    auto jValues = makeStepVector(minPt.y, graphResolution, maxPt.y);
+
+    pcl::PointXYZ searchPoint;
+    searchPoint.x = 0.0f;
+    searchPoint.y = 0.0f;
+    searchPoint.z = 0.0f;
+
+    int index = 0;
+
+    for (int j = columns - 1; j >= 0; j--) {
+        for (int i = 0; i < rows; i++) {
+            searchPoint.x = (float) iValues[i];
+            searchPoint.y = (float) jValues[j];
+            octree.nearestKSearch(searchPoint, 1, pointIdxNKNSearch, pointNKNSquaredDistance);
+            graphCoordinates.at(index, 1) = (*cloud)[pointIdxNKNSearch[0]].x;
+            graphCoordinates.at(index, 2) = (*cloud)[pointIdxNKNSearch[0]].y;
+            graphCoordinates.at(index, 3) = (*cloud)[pointIdxNKNSearch[0]].z;
+            index += 1;
+        }
+    }
+}
+
+float dist3D(float p1_x, float p1_y, float p1_z, float p2_x, float p2_y, float p2_z) {
+    return sqrt(pow(p1_x - p2_x, 2) + pow(p1_y - p2_y, 2) + pow(p1_z - p2_z, 2) * 1.0);
+}
+
+void calculateCostMatrix(Matrix &AdjacencyMatrix, Matrix &CostMatrix, Matrix &graphCoordinates, int N) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (AdjacencyMatrix.at(i, j) == 1) {
+                CostMatrix.at(i, j) = dist3D(
+                        graphCoordinates.at(i, 1), graphCoordinates.at(i, 2), graphCoordinates.at(i, 3),
+                        graphCoordinates.at(j, 1), graphCoordinates.at(j, 2), graphCoordinates.at(j, 3));
+            }
+        }
     }
 }
 
@@ -88,13 +128,14 @@ int main() {
     auto start = high_resolution_clock::now();
 
     float octreeResolution = 0.1f; // Resolução da octree
-    float graphResolution = 0.6f; // Resolução do grafo em metros
+    float graphResolution = 0.8f; // Resolução do grafo em metros
 
     PointCloud cloud = importPointCloud();
     Octree octree = transformPointCloudToOctree(cloud, octreeResolution);
 
-    coder::array<float, 2> AdjacencyMatrix;
-    coder::array<float, 2> CostMatrix;
+    Matrix AdjacencyMatrix;
+    Matrix CostMatrix;
+    Matrix graphCoordinates;
 
     int rows = 0;
     int columns = 0;
@@ -103,24 +144,14 @@ int main() {
 
     AdjacencyMatrix.set_size(N, N);
     CostMatrix.set_size(N, N);
+    graphCoordinates.set_size(N, 3);
 
     populateAdjacencyMatrix(AdjacencyMatrix, rows, columns);
+    getGraphCoordinates(octree, cloud, graphCoordinates, rows, columns, graphResolution);
+    calculateCostMatrix(AdjacencyMatrix, CostMatrix, graphCoordinates, rows * columns);
 
-    double resultado = Dijkstra(AdjacencyMatrix, CostMatrix, 1200, 1500);
-
+    double resultado = Dijkstra(AdjacencyMatrix, CostMatrix, 50, 55);
     std::cout << "Custo total: " << resultado << std::endl;
-
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 
     auto stop = high_resolution_clock::now();
     std::chrono::duration<double, std::milli> duration = stop - start;
@@ -128,13 +159,6 @@ int main() {
               << duration.count() / 1000
               << " segundos para executar."
               << std::endl;
-
-//    int K = 10;
-//    pcl::PointXYZ searchPoint;
-//    searchPoint.x = 6.25f;
-//    searchPoint.y = -14.15f;
-//    searchPoint.z = 0.35f;
-//    getKNearestNeighboursFromOctree(octree, cloud, K, searchPoint);
 
     return 0;
 }
